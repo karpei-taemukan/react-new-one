cfd00afd674c9d39961b1f254eebd182d071f65c
karpeitaemukan <l07wkdgustn07l@gmail.com>
2022-11-08 00:39:10 +0900

    settings todolist

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import {DefaultTheme} from "styled-components";
+
+export const darkTheme:DefaultTheme = {
+    textColor: "#f5f6fa",
+    bgColor: "#2f3640",
+    accentColor:"#44bd32"
+}
+
+export const lightTheme:DefaultTheme = {
+    textColor: "#2f3640",
+    bgColor: "#f5f6fa",
+    accentColor:"#44bd32"
+}
